faf89adcfd502b91f10d5f04ee7539af09281f80
Hammer <hammer@pop-os.localdomain>
2021-05-04 16:10:03 +0530

    added Arrow Based Navigation and Scrolling

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 let input = document.getElementById('input');
 let list  = document.getElementById('list');
+let display_area= document.getElementById('display_area');
+
+let selected=0;
+let list_length;
+let array;
 
 
 let database =[
@@ -30,6 +35,20 @@ let feature =(key)=>{
     }
     return arr;
 }
+
+let renderList =(list_array,index)=>{
+    list_length=list_array.length;
+    array=list_array;
+    for(i=0;i<list_array.length;i++){
+        if(i==index){
+            list.innerHTML+='<li id="current">'+list_array[i]+'</li>'
+        }
+        else{
+
+            list.innerHTML+='<li>'+list_array[i]+'</li>'
+        }
+    }
+}
 input.addEventListener('keyup',(event)=>{
     let key=input.value.toLowerCase();
 
@@ -39,15 +58,42 @@ input.addEventListener('keyup',(event)=>{
         list.innerHTML='';
         list_array=feature(key);
 
-        for(i=0;i<list_array.length;i++){
-            list.innerHTML+='<a target=”_blank” href="https://www.google.com/search?q='+list_array[i]+'"><li>'+list_array[i]+'</li></a>'
-        }
+        renderList(list_array,selected);
     }
     else{
         list.innerHTML='';
     }
-
 });
+
+y=0;
+document.addEventListener('keydown', function(e) {
+    let code=e.keyCode
+    if(code==38){
+        if(selected-1>=0){
+            y-=50;
+            window.scrollTo(0, y);
+            selected--;
+        }
+    }
+    else if(code==40){
+        if(selected+1<list_length){
+            window.scrollTo(0, y);
+            y+=50;
+            selected++;
+        }
+    }
+    else if(code==13){
+        display_area.innerHTML='';
+        display_area.innerHTML+=array[selected];
+        y=0;
+        window.scrollTo(0, y);
+
+    }
+    else if(code==8){
+        selected=0;
+    }
+});
+
 
 
 
